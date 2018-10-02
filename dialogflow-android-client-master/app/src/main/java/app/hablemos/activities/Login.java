991cc7a7ab3d6ef748aabd10ba7f98225cb3b321
carolinaGalassi <galassi.carolina@gmail.com>
2018-10-02 20:01:23 -0300

    pantalla  login bonita

---
+++ dialogflow-android-client-master/app/src/main/java/app/hablemos/activities/Login.java
@@ -0,0 +1,15 @@
+package app.hablemos.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import app.hablemos.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
