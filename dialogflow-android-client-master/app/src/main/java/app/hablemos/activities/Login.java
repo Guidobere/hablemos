0876114a8856b2f516c3e4ad7a9bb7fbe18873ec
Guido Berezovsky <gberezovsky@intranet.certant.com>
2018-10-04 15:56:59 -0300

    actualizo el repo con los cambios

--- dialogflow-android-client-master/app/src/main/java/app/hablemos/activities/Login.java
+++ dialogflow-android-client-master/app/src/main/java/app/hablemos/activities/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,21 +26,33 @@ public class Login extends AppCompatActivity {
     private TextView txtRegistrar;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseAuth.AuthStateListener mAuthListener;
+    //private FirebaseAuth auth;
 
-    private String TAG = getString(R.string.tagLogin);
+    private String TAG;
+    private  String password;
+    private  String email;
+
+    private EditText mailTxtBox;
+    private EditText passTxtBox;
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        //LOGICA BOTON LOGIN
-        Button button = findViewById(R.id.Ingresar);
-        button.setOnClickListener(new View.OnClickListener() {
+        TAG = getString(R.string.tagLogin);
+
+        btnLogin = findViewById(R.id.Ingresar);
+        mailTxtBox = findViewById(R.id.txtUser);
+        passTxtBox = findViewById(R.id.txtPass);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-                singIn();
+                // Code here executes on main thread after user presses btnLogin
                 //loguearUsuario();
                 //crearUsuario(user,pass);
+                singIn();
             }
         });
 
@@ -54,111 +67,112 @@ public class Login extends AppCompatActivity {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
                 }
-                // ...
             }
         };
 
         //REGISTRARSE
-        txtRegistrar = (TextView) findViewById(R.id.textRegistro);
+        txtRegistrar = findViewById(R.id.textRegistro);
         txtRegistrar.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                //startActivity(RegistroActivity.class);
-                crearUsuario();
+                startActivity(RegistroActivity.class);
+               // crearUsuario();
             }
         });
     }
 
 /*    private void login(){
-        EditText userTxtBox = (EditText) findViewById(R.id.txtUser);
+        EditText mailTxtBox = (EditText) findViewById(R.id.txtUser);
         EditText passTxtBox = (EditText) findViewById(R.id.txtPass);
-
-        final String email = userTxtBox.getText().toString();
+        final String email = mailTxtBox.getText().toString();
         final String password = passTxtBox.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(Login.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    //updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    //updateUI(null);
+                }
+            }
+        });
     }*/
 
     //CREAR O LOGEAR USUARIO
     private void crearUsuario() {
-        EditText userTxtBox = (EditText) findViewById(R.id.txtUser);
-        EditText passTxtBox = (EditText) findViewById(R.id.txtPass);
+        email = mailTxtBox.getText().toString();
+        password = passTxtBox.getText().toString();
 
-        final String email = userTxtBox.getText().toString();
-        final String password = passTxtBox.getText().toString();
-        mAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success");
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        startActivity(MainActivity.class);
-                        //updateUI(user);
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                        Toast.makeText(Login.this, getString(R.string.fallo_autenticacion),
-                                Toast.LENGTH_SHORT).show();
-                        //updateUI(null);
-                    }
-                    // ...
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmailAndPassword:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    startActivity(MainActivity.class);
+                    //updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmailAndPassword:failure", task.getException());
+                    Toast.makeText(Login.this, getString(R.string.fallo_autenticacion), Toast.LENGTH_SHORT).show();
+                    //updateUI(null);
                 }
-            });
+            }
+        });
     }
 
     private void startActivity(Class<?> cls) {
         final Intent intent = new Intent(this, cls);
+        Bundle mBundle = new Bundle();
+        mBundle.putString("1",email);
+        intent.putExtras(mBundle);
         startActivity(intent);
     }
 
-    //Create a new createAccount method which takes in an email address and password, validates them and then creates a new user with the createUserWithEmailAndPassword method.
     private void singIn(){
-        EditText userTxtBox = (EditText) findViewById(R.id.txtUser);
-        EditText passTxtBox = (EditText) findViewById(R.id.txtPass);
-        String email = userTxtBox.getText().toString();
-        String password = passTxtBox.getText().toString();
+        email = mailTxtBox.getText().toString();
+        password = passTxtBox.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success");
-                        FirebaseUser user = mAuth.getCurrentUser();
-                        startActivity(MainActivity.class);
-                        //updateUI(user);
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Coloque direccion de email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Coloque una contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    startActivity(MainActivity.class);
+                    //updateUI(user);
+                } else {
+                    if (password.length() < 6) {
+                        passTxtBox.setError(getString(R.string.minimum_password));
                     } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.getException());
-                        Toast.makeText(Login.this, getString(R.string.fallo_autenticacion),
-                                Toast.LENGTH_SHORT).show();
-                        //updateUI(null);
+                        Toast.makeText(Login.this, getString(R.string.fallo_autenticacion), Toast.LENGTH_LONG).show();
                     }
-                    // ...
+                    // If sign in fails, display a message to the user.
+                    //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    //Toast.makeText(Login.this, getString(R.string.fallo_autenticacion), Toast.LENGTH_SHORT).show();
+                    //updateUI(null);
                 }
-            });
+            }
+        });
     }
 
     @Override
