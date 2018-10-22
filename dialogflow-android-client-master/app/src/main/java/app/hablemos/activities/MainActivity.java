576a69d862ebe62615ea766628e3c6417dd2fc6c
maxmouse <bolia.maximiliano@gmail.com>
2018-10-22 14:36:14 -0300

    se cambia el mensaje de los recordatorios de presion y glucosa

--- dialogflow-android-client-master/app/src/main/java/app/hablemos/activities/MainActivity.java
+++ dialogflow-android-client-master/app/src/main/java/app/hablemos/activities/MainActivity.java
@@ -589,12 +589,12 @@ public class MainActivity extends AppCompatActivity implements AIListener , View
                     if(elturno.equalsIgnoreCase(turnoPresion) && losdias.contains(diaSemana)){
                         a=1;
                        // speech="Hoy si";
-                        loQueDiceYescribe("Hoy si","default");
+                        loQueDiceYescribe("Hoy a la "+ turnoPresion +" debes medirte la presion","default");
                     }
                 }
 
                 if(a == 0) {
-                    loQueDiceYescribe("Hoy no","default");
+                    loQueDiceYescribe("Hoy a la "+ turnoPresion +" no debes medirte la presion","default");
                 }
             }
 
@@ -624,12 +624,12 @@ public class MainActivity extends AppCompatActivity implements AIListener , View
 
                     if(elturno.equalsIgnoreCase(turnoGlucosa) && losdias.contains(diaSemana)){
                         a=1;
-                       loQueDiceYescribe("Hoy si","default");
+                       loQueDiceYescribe("Hoy a la "+ turnoGlucosa +" debes medirte la glucosa","default");
                     }
                 }
 
                 if(a == 0) {
-                     loQueDiceYescribe("Hoy no","default");
+                     loQueDiceYescribe("Hoy a la "+ turnoGlucosa +" no debes medirte la glucosa","default");
                 }
             }
 
